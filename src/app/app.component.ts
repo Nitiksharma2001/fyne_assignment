08dc37be57d0f66fe9d5a14c009ada979b3be536
Nitik Sharma <nitiksharma.job@gmail.com>
2024-07-18 14:59:41 +0530

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddWorkoutComponent } from './components/add-workout/add-workout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddWorkoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/add-workout/add-workout.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-workout',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-workout.component.html',
+  styleUrl: './add-workout.component.css'
+})
+export class AddWorkoutComponent {
+  userName: string
+  constructor(){
+    this.userName = ''
+  }
+
+}
